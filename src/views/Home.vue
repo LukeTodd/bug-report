bee49a6fa6ccd8d4bddc4bf159237098be19de42
Luke Todd <luke.todd22@yahoo.com>
2019-03-10 11:18:56 -0600

    added drop down

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,6 +9,19 @@
     <div class="row">
       <bug-form></bug-form>
     </div>
+    <div class='col-3'>
+      <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown"
+          aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+          <button class="dropdown-item" type="button">Action</button>
+          <button class="dropdown-item" type="button">Another action</button>
+          <button class="dropdown-item" type="button">Something else here</button>
+        </div>
+      </div>
+    </div>
     <div class="row">
       <table class='table t-display'>
         <thead>
